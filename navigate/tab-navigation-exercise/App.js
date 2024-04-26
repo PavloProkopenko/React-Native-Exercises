a0bc5bf298caac9dbc78e9797cb0ced3eefef699
Pavlo <stpasha6@gmail.com>
2024-04-26 21:59:13 +0300

    Other examples of navigation

---
+++ navigate/tab-navigation-exercise/App.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './WelcomeScreen';
+import LoginScreen from './LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        <View style={styles.container}>
+          <LittleLemonHeader />
+          <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Login') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Welcome') {
+            iconName =  'ios-list';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}>
+            <Tab.Screen name="Welcome" component={WelcomeScreen} />
+            <Tab.Screen name="Login" component={LoginScreen} />
+          </Tab.Navigator>
+        </View>
+        <View style={styles.footerContainer}>
+          <LittleLemonFooter />
+        </View>
+      </NavigationContainer>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#333333',
+  },
+  footerContainer: { backgroundColor: '#333333' },
+});
+
